7f45b34c7a5eec1284bcf72877b2105ecbac92ec
Marcelo Perez Izco <marceloperezizco@gmail.com>
2025-02-16 21:52:05 +0000

    Setting environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
     entry: "./src/index.js",
@@ -55,6 +56,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "assets/css/[name].[contenthash].css", // Ubicamos CSS en "assets/css/"
         }),
+        new Dotenv(),
     ],
     optimization: {
         minimize: true,
