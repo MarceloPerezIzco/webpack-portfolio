4756e0a4b8dd83956cdc45df92b2baf1e0f80ae9
Marcelo Perez Izco <marceloperezizco@gmail.com>
2025-02-16 22:21:52 +0000

    Setting Webpack file for development mode

---
+++ webpack.config.dev.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "assets/js/[name].[contenthash].js", // Ubicamos JS en "assets/js/"
+        assetModuleFilename: "assets/images/[hash][ext][query]", // Para imágenes con hash
+    },
+    mode: "development",
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            "@utils": path.resolve(__dirname, "src/utils/"),
+            "@templates": path.resolve(__dirname, "src/templates/"),
+            "@styles": path.resolve(__dirname, "src/styles/"),
+            "@images": path.resolve(__dirname, "src/assets/images/"),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+            },
+            {
+                test: /\.png/,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/fonts/[name].[contenthash][ext]",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./public/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "assets/css/[name].[contenthash].css", // Ubicamos CSS en "assets/css/"
+        }),
+        new Dotenv(),
+    ],
+};
