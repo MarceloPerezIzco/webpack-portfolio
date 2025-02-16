431e7c8359753f85f1f1c2be78087d65b3aeb0c6
Marcelo Perez Izco <marceloperezizco@gmail.com>
2025-02-16 16:48:51 +0000

    Optimisation: Hashes, Compression and File Minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        assetModuleFilename: "assets/images/[hash][ext][query]",
+        filename: "assets/js/[name].[contenthash].js", // Ubicamos JS en "assets/js/"
+        assetModuleFilename: "assets/images/[hash][ext][query]", // Para imágenes con hash
     },
     resolve: {
         extensions: [".js"],
@@ -32,6 +34,9 @@ module.exports = {
             {
                 test: /\.(woff|woff2)$/,
                 type: "asset/resource",
+                generator: {
+                    filename: "assets/fonts/[name].[contenthash][ext]",
+                },
             },
         ],
     },
@@ -41,6 +46,12 @@ module.exports = {
             template: "./public/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "assets/css/[name].[contenthash].css", // Ubicamos CSS en "assets/css/"
+        }),
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    },
 };
